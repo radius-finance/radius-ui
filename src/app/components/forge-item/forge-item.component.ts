aeb6517d6b4880c54a6088e1a116446234da5f1d
cryptoleadfoot <cryptoleadfoot108@protonmail.com>
2021-03-11 18:48:26 -0700

    updates

--- src/app/components/forge-item/forge-item.component.ts
+++ src/app/components/forge-item/forge-item.component.ts
@@ -12,14 +12,13 @@ export class ForgeItemComponent implements OnInit, OnDestroy {
   catalystAmount: number;
   catalystOptions: Options = {
     floor: 0,
-    step: 0.1,
     ceil: 1,
   };
   forgeAmount: number;
   forgeOptions: Options = {
     floor: 1,
     step: 1,
-    ceil: 255,
+    ceil: 10,
   };
   constructor(private blockchainService: BlockchainService) {}
 
@@ -37,7 +36,6 @@ export class ForgeItemComponent implements OnInit, OnDestroy {
         this.blockchainService.formatEther(balances.gas.erc20)
       );
       this.forgeOptions.floor = this.forgeOptions.ceil !== 0 ? 1 : 0;
-      this.forgeAmount = this.forgeOptions.floor;
     };
     // register the handler above with the blockchain service && call it to update UI
     this.blockchainService.addToUpdateList(this.updateSliderBounds);
